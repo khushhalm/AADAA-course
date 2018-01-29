16d829590045fe0ea715583808224392a33ced59
khushhalm <khushhal000maheshwari@gmail.com>
2018-01-29 12:01:07 +0530

    imporvement in heap.c

--- heap.c
+++ heap.c
@@ -31,7 +31,7 @@ void insert(int no)
 
 void option_0()
 {
-	printf("How many elements you want to add?\n");
+	printf("How many elements you want to insert?\n");
     scanf("%i", &size);
     printf("%i\n", size);
     
@@ -42,7 +42,7 @@ void option_0()
     
     int no, iter;
     for(iter = 1; iter <= size; iter++) {
-        printf("Enter next elements you want to add to the heap!:  ");
+        printf("Enter next elements you want to insert to the heap!:  ");
         scanf("%i", &no);
         insert(no);
         //printf("%i, %i \n", iter, size);
@@ -52,14 +52,14 @@ void option_0()
 
 void option_1()
 {
-    printf("How many elements you want to add?\n");
+    printf("How many elements you want to insert?\n");
     int new_size;
     scanf("%i", &new_size);
     size += new_size;
     //printf("%i\n", size);
     int no, iter;
     for(iter = 1; iter <= new_size; iter++) {
-        printf("Enter next elements you want to add to the heap!:  ");
+        printf("Enter next elements you want to insert to the heap!:  ");
         scanf("%i", &no);
         insert(no);
     }
@@ -75,15 +75,68 @@ void option_2()
 	int no = heap[size];
 	size-=1;
 	int now = size/2;
-    while (heap[now] < no  && now>=1 &&now<=size)
+	int now_even;
+	if (now==1 && size==2)
+	{
+		//now = 1;
+		now_even = size;
+		printf("\nnow=%i and now_even=%i\n",now, now_even);	
+	}
+	else if (now==1 && size==3)
+	{
+		now = 2;
+		now_even = size;
+		printf("\nnow=%i and now_even=%i\n",now, now_even);	
+	}
+	else if (now==2 && size==4)
+	{
+		//now = 2;
+		now_even = 3;
+		printf("\nnow=%i and now_even=%i\n",now, now_even);	
+	}	
+	else if (size%2==0)
+	{
+		now_even = size/2-1;
+		printf("\nnow=%i and now_even=%i\n",now, now_even);	
+	}
+	else
+	{
+		now_even = size/2+1;
+		printf("\nnow=%i and now_even=%i\n",now, now_even);
+	}
+	heap_size=size;
+	int new_no = 1;
+	while (now>=1 && now<=size && now_even>=1 && now_even<=size)
     {
-        heap[now/2] = heap[now];
-        now *= 2;
-        //printf("\nnow is %i",now);
-        //print_array();
+    	if (heap[now]<no && heap[now]<=heap[now_even])
+    	{
+		    heap[now/2] = heap[now];
+		    //no = heap[now];
+		    new_no = now;
+		    now *= 2;
+		    now_even = now+1;
+		    printf("\nnow is %i",now);
+		    print_array();
+        }
+        else if (heap[now_even]<no && heap[now]>=heap[now_even])
+        {
+		    heap[now_even/2] = heap[now_even];
+		    //no = heap[now_even];
+		    new_no = now_even;
+		    now_even *= 2;
+		    now = now_even+1;
+		    printf("\nnow_even is %i",now_even);
+		    print_array();
+		}
+        else
+        {
+        	break;
+        }
+        printf("\nnow=%i and now_even=%i and \n",now, now_even);
     }
-    heap[size]=no;
-    //printf("\nnow is %i, heap[now] is %i\n",now, heap[now]);
+    
+    heap[new_no]=no;
+    printf("\nnow is %i, heap[now] is %i\n",now, heap[now]);
     print_array();
 }
 void option_3()
